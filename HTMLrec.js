82ba7b1b4ddbe776797e6635aa7e2f61227a6cb7
andychadwick <30505601+andychadwick@users.noreply.github.com>
2017-07-27 18:15:28 +0100

    Add files via upload

---
+++ HTMLrec.js
@@ -0,0 +1,175 @@
+(function (lib, img, cjs, ss, an) {
+
+var p; // shortcut to reference prototypes
+lib.webFontTxtInst = {}; 
+var loadedTypekitCount = 0;
+var loadedGoogleCount = 0;
+var gFontsUpdateCacheList = [];
+var tFontsUpdateCacheList = [];
+lib.ssMetadata = [
+		{name:"HTMLrec_atlas_", frames: [[0,0,950,555],[608,557,302,325],[0,557,302,325],[0,884,302,325],[304,557,302,325]]}
+];
+
+
+
+lib.updateListCache = function (cacheList) {		
+	for(var i = 0; i < cacheList.length; i++) {		
+		if(cacheList[i].cacheCanvas)		
+			cacheList[i].updateCache();		
+	}		
+};		
+
+lib.addElementsToCache = function (textInst, cacheList) {		
+	var cur = textInst;		
+	while(cur != exportRoot) {		
+		if(cacheList.indexOf(cur) != -1)		
+			break;		
+		cur = cur.parent;		
+	}		
+	if(cur != exportRoot) {		
+		var cur2 = textInst;		
+		var index = cacheList.indexOf(cur);		
+		while(cur2 != cur) {		
+			cacheList.splice(index, 0, cur2);		
+			cur2 = cur2.parent;		
+			index++;		
+		}		
+	}		
+	else {		
+		cur = textInst;		
+		while(cur != exportRoot) {		
+			cacheList.push(cur);		
+			cur = cur.parent;		
+		}		
+	}		
+};		
+
+lib.gfontAvailable = function(family, totalGoogleCount) {		
+	lib.properties.webfonts[family] = true;		
+	var txtInst = lib.webFontTxtInst && lib.webFontTxtInst[family] || [];		
+	for(var f = 0; f < txtInst.length; ++f)		
+		lib.addElementsToCache(txtInst[f], gFontsUpdateCacheList);		
+
+	loadedGoogleCount++;		
+	if(loadedGoogleCount == totalGoogleCount) {		
+		lib.updateListCache(gFontsUpdateCacheList);		
+	}		
+};		
+
+lib.tfontAvailable = function(family, totalTypekitCount) {		
+	lib.properties.webfonts[family] = true;		
+	var txtInst = lib.webFontTxtInst && lib.webFontTxtInst[family] || [];		
+	for(var f = 0; f < txtInst.length; ++f)		
+		lib.addElementsToCache(txtInst[f], tFontsUpdateCacheList);		
+
+	loadedTypekitCount++;		
+	if(loadedTypekitCount == totalTypekitCount) {		
+		lib.updateListCache(tFontsUpdateCacheList);		
+	}		
+};
+// symbols:
+
+
+
+(lib.background = function() {
+	this.spriteSheet = ss["HTMLrec_atlas_"];
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.esme = function() {
+	this.spriteSheet = ss["HTMLrec_atlas_"];
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.esme_blink = function() {
+	this.spriteSheet = ss["HTMLrec_atlas_"];
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.esme_speak2 = function() {
+	this.spriteSheet = ss["HTMLrec_atlas_"];
+	this.gotoAndStop(3);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.esme_tap = function() {
+	this.spriteSheet = ss["HTMLrec_atlas_"];
+	this.gotoAndStop(4);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.EsmeBlink = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// timeline functions:
+	this.frame_64 = function() {
+		/* gotoAndPlay(3)*/
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).wait(64).call(this.frame_64).wait(1));
+
+	// Layer 1
+	this.instance = new lib.esme_speak2();
+	this.instance.parent = this;
+
+	this.instance_1 = new lib.esme();
+	this.instance_1.parent = this;
+	this.instance_1._off = true;
+
+	this.instance_2 = new lib.esme_blink();
+	this.instance_2.parent = this;
+
+	this.instance_3 = new lib.esme_tap();
+	this.instance_3.parent = this;
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},2).to({state:[{t:this.instance_2}]},19).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_2}]},19).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_3}]},12).to({state:[{t:this.instance_1}]},2).to({state:[{t:this.instance_3}]},2).to({state:[{t:this.instance_1}]},2).to({state:[{t:this.instance_3}]},2).to({state:[{t:this.instance_2}]},2).wait(1));
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(2).to({_off:false},0).to({_off:true},19).wait(1).to({_off:false},0).to({_off:true},19).wait(1).to({_off:false},0).to({_off:true},12).wait(2).to({_off:false},0).to({_off:true},2).wait(2).to({_off:false},0).to({_off:true},2).wait(3));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,302,325);
+
+
+// stage content:
+(lib.Untitled2 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 1
+	this.instance = new lib.EsmeBlink();
+	this.instance.parent = this;
+	this.instance.setTransform(612,281,1,1,0,0,0,151,162.5);
+
+	this.instance_1 = new lib.background();
+	this.instance_1.parent = this;
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance_1},{t:this.instance}]}).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(475,277.5,950,555);
+// library properties:
+lib.properties = {
+	width: 950,
+	height: 555,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	webfonts: {},
+	manifest: [
+		{src:"images/HTMLrec_atlas_.png", id:"HTMLrec_atlas_"}
+	],
+	preloads: []
+};
+
+
+
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{}, AdobeAn = AdobeAn||{});
+var lib, images, createjs, ss, AdobeAn;
